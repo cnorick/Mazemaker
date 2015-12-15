e899079195170b5d3aad72adbff2b852a435fefd
Cody_Nathan_Orick <cnathanorick@gmail.com>
2015-12-15 14:38:37 -0500

    Add install script. renamed/updated game.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# downloads the required files, make the exacutable, and deletes the downloaded files
+
+URL="https://github.com/cnorick/Mazemaker.git"
+folder="/tmp/Mazesolver"
+currentDir=`pwd`
+
+mkdir Mazemaker_support
+
+echo downloading source
+git clone $URL $folder
+
+cd $folder
+make
+
+cp game.sh $currentDir
+cp mazemake mazesolve mazeshow $currentDir/Mazemaker_support
+
+cd $curentDir
+
+bash game.sh
+
+echo deleting source
+rm -rf $folder
--- maze.sh
+++ maze.sh
@@ -2,9 +2,15 @@
 
 R=$1
 C=$2
+folder=./Mazemaker_support
+
+cd $folder
 
 ./mazemake ${R:=60} ${C:=80} > maze.txt
 ./mazesolve < maze.txt > path.txt 
 
 ./mazeshow 10 maze.txt | convert - maze.png
 ./mazeshow 10 maze.txt path.txt | convert - maze_path.png
+
+mv maze.png maze_path.png ..
+rm maze.txt path.txt
