d2ecf07e4cf48cf34b61831a29fe438a195ac7d3
Cody_Nathan_Orick <cnathanorick@gmail.com>
2015-12-16 14:46:42 -0500

    Adds dependency installer install.sh now automatically installs dependencies and updates packages fixes #23

--- install.sh
+++ install.sh
@@ -1,46 +1,64 @@
 # downloads the required files, make the exacutable, and deletes the downloaded files
 
 URL="https://github.com/cnorick/Mazemaker.git"
-folder="/tmp/Mazesolver"
+tmp="/tmp/Mazesolver"
 Dir="/opt/Mazemaker"
 
 
-install(){
-	mkdir $Dir;
-	cd $Dir;
-	mkdir mazemaker_support;
+install_log(){
+	echo -e "\n\033[1;32mMAZEMAKER INSTALL: $*\033[m";
+}
 
-	echo downloading source...;
-	git clone $URL $folder;
-	cd $folder/source;
-	make;
+install_error(){
+	echo -e "\n\033[1;37;41mMAZEMAKER INSTALL ERROR: $*\033[m";
+	exit 1;
+}
 
-	cp mazemake mazesolve mazeshow $Dir/mazemaker_support;
+create_directories(){
+	install_log "Creating Mazemaker directories";
+	if [ ! -d "$Dir/mazemaker_support" ]; then
+		sudo mkdir -p "$Dir/mazemaker_support" || 
+			install_error "Couldn't create directory '$Dir/mazemaker_support'";
+	fi
+}
+
+compile_source(){
+	install_log "Compiling source";
+	
+	if [ -d $tmp ]; then
+		sudo rm -rf $tmp;
+	fi;
+
+	sudo git clone $URL $tmp;
+	cd $tmp/source;
+	sudo make;
+
+	sudo cp mazemake mazesolve mazeshow $Dir/mazemaker_support;
 	cd ..;
-	cp mazemaker $Dir;
+	sudo cp mazemaker $Dir;
 
-	ln -s $Dir/mazemaker /bin;
-
-	echo deleting source...;
-	rm -rf $folder;
-}
-
-clean(){
-	if [ -e $Dir ]; then
-		rm -fr $Dir;
+	if [ ! -L /bin/mazemaker ]; then
+		sudo ln -s $Dir/mazemaker /bin;
 	fi;
 
-	if [ -L /bin/mazemaker ]; then
-		rm -fr /bin/mazemaker;
-	fi;
+	sudo rm -rf $tmp;
+}
+
+update_system_packages(){
+	install_log "Updating packages";
+	sudo apt-get update || install_error "Couldn't update package list";
+	sudo apt-get upgrade || install_error "Couldn't upgrade packages";
+}
+
+install_dependencies(){
+	install_log "Installing required packages";
+	sudo apt-get install imagemagick build-essential || 
+		install_error "Couldn't install dependencies";
 }
 
 
 
-if [ $USER != "root" ]; then
-echo must run as root;
-exit;
-fi;
-
-clean;
-install;
+create_directories;
+update_system_packages;
+install_dependencies;
+compile_source;
